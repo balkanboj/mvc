b000e04110df831efd78c6b7bd39021c2e227f96
Emran Dzanic <emrandz05@gmail.com>
2025-04-11 00:30:31 +0200

    MVC start

--- me/report/src/Controller/WebsiteController.php
+++ me/report/src/Controller/WebsiteController.php
@@ -4,21 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use DateTime; 
 
 class WebsiteController extends AbstractController
 {
-    // #[Route("/lucky/number/twig", name: "lucky_number")]
-    // public function number(): Response
-    // {
-    //     $number = random_int(0, 100);
-
-    //     $data = [
-    //         'number' => $number
-    //     ];
-
-    //     return $this->render('lucky_number.html.twig', $data);
-    // }
 
     #[Route("/", name: "homepage")]
     public function home(): Response
@@ -45,8 +36,51 @@ class WebsiteController extends AbstractController
     }
 
     #[Route("/lucky", name: "lucky")]
-    public function lucky(): Response
+    public function number(): Response
     {
-        return $this->render('lucky.html.twig');
+        $number = random_int(0, 100);
+
+        $data = [
+            'number' => $number
+        ];
+
+        return $this->render('lucky.html.twig', $data);
+    }
+
+    #[Route("/api", name: "api")]
+    public function apiLanding(): Response
+    {
+        return $this->render('api.html.twig');
+    }
+
+    #[Route('/api/quote', name: 'api_quote')]
+    public function quote(): JsonResponse
+    {
+
+        $quotes = [
+            "Careful, man, theres a beverage here!",
+            "Im the dude. So thats what you call me. You know, that or, uh, his dudeness, duder, or El duderino if youre not into that whole brevity thing.",
+            "Youre being very un-dude."
+        ];
+
+        $randomQuote = $quotes[array_rand($quotes)];
+
+        $date = new DateTime();
+        $todayDate = $date->format('Y-m-d');
+
+        $timestamp = $date->getTimestamp();
+        $formattedDate = $date->format('H:i:s');
+
+        $data = [
+            'quote' => $randomQuote,
+            'date' => $todayDate,
+            'timestamp' => $formattedDate,
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
     }
 }
